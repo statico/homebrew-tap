017e27ff9f412b67a3562be24f7010174173d1a3
Anka <runner@sjc22-be105-14387be2-52f8-4404-8ee4-ad428ea7cc90-56344600725E.local>
2026-09-19 06:11:30 +0000

    macotron 0.7.0

--- Casks/macotron.rb
+++ Casks/macotron.rb
@@ -1,6 +1,6 @@
 cask "macotron" do
-  version "0.6.1"
-  sha256 "a4e3e68681fa09343430ba5bb0005a29ff2b336af10a20f552607b38a834c480"
+  version "0.7.0"
+  sha256 "9e82d5b3f3fb10415526f421b666029c7d4954acc599b892d8c6ff23e6ba78b7"
 
   url "https://github.com/statico/macotron/releases/download/v#{version}/Macotron-#{version}.dmg"
   name "Macotron"
